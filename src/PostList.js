ee03d4fd442468d311707c5b9b527d3b0ac6f5a1
lanzarot <luis-389@hotmail.com>
2023-08-06 21:39:17 -0500

    comments component

--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 export default () => {
     const [posts, setPosts] = useState({});
@@ -15,15 +17,17 @@ export default () => {
     }, []);
 
     // console.log(posts);
-    const renderedPosts = Object.values(posts).map( posts => {
+    const renderedPosts = Object.values(posts).map( post => {
         return (
             <div 
                 className="card" 
                 style={{ width: '30%', marginBottom: '20px' }}
-                key={posts.id}
+                key={post.id}
             >
                 <div className="card-body">
-                    <h3>{posts.title}</h3>
+                    <h3>{post.title}</h3>
+                    <CommentList postId={post.id}/>
+                    <CommentCreate postId={post.id} />
                 </div>
             </div>
         );
